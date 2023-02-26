b91d1f0112f447ae1dcbddfcd1c996124ac91444
juanp <juanpcandal@gmail.com>
2023-02-26 19:37:54 -0300

    models

--- goles/models.py
+++ goles/models.py
@@ -1,10 +1,11 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
 
 class Goles(models.Model):
 
-    fecha = models.DateField('%Y-%m-%d')
+    fecha = models.DateField('%Y-%m-%d', default=timezone.now)
     hora = models.TimeField()
     equipo = models.CharField(max_length=30)
     rival = models.CharField(max_length=30)
