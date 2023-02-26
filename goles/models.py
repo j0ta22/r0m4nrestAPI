7d88b2c1696d1d5b1abc9a9e2e3ab1c64bcb8896
juanp <juanpcandal@gmail.com>
2023-02-26 19:52:25 -0300

    removing cache

--- goles/models.py
+++ goles/models.py
@@ -1,11 +1,11 @@
-from django.utils import timezone
+
 from django.db import models
 
 # Create your models here.
 
 class Goles(models.Model):
 
-    fecha = models.DateField('%Y-%m-%d', default=timezone.now)
+    fecha = models.DateField('%Y-%m-%d')
     hora = models.TimeField()
     equipo = models.CharField(max_length=30)
     rival = models.CharField(max_length=30)
